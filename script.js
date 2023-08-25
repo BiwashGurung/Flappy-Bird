7e52d0c496ffb18ef8e27f21aff22c576bb21b40
BiwashGurung <varungrg10@gmail.com>
2023-08-25 17:49:09 +0545

    editing the canvas width and height

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
-canvas.width = 300;
-canvas.height = 635;
+canvas.width = 256;
+canvas.height = 512;
 
 let bird = new Image();
 let backround = new Image();
